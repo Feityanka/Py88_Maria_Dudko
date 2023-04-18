5109dd870692e7347be99da9cd48f7e55bedb2e0
Feityanka <feityanka@mail.ru>
2023-04-19 02:48:49 +0300

    Added a wrapper function!

---
+++ Lesson6/decorator.py
@@ -0,0 +1,32 @@
+def some_decorator(what_time):
+    def the_wrapper():
+        from datetime import datetime
+        current_datetime = datetime.now()
+        time = datetime.now()
+        print(time)
+        what_time()
+        print(time)
+    return the_wrapper
+
+
+@some_decorator
+def some_function():
+    username = input('Hello! What is your name?''\n')
+    print(f'Hello, {username}!')
+    return username
+
+
+some_function()
+
+
+@some_decorator
+def more_questions():
+    decision = input('Nice! And mine is Mary. Do you like cats?''\n')
+    if decision == 'Yes' or decision == 'yes' or decision == 'y' or decision == 'Y':
+        print('Yay! Same')
+    else:
+        print('Boo-hoo! And I do love them!')
+    return decision
+
+
+questionmark = more_questions()
